b4c8b0d7d5125d39d700a6ba6c5e9a016098b2f7
Sebastian Quek <sebq13@gmail.com>
2022-12-06 11:24:35 +0800

    Use BDD for utils tests

--- tests/utils_test.ts
+++ tests/utils_test.ts
@@ -1,6 +1,14 @@
+import { configSync } from "https://deno.land/std@0.166.0/dotenv/mod.ts";
+import {
+  afterAll,
+  beforeAll,
+  describe,
+  it,
+} from "https://deno.land/std@0.166.0/testing/bdd.ts";
 import {
   assertSpyCalls,
   resolvesNext,
+  Stub,
   stub,
 } from "https://deno.land/std@0.166.0/testing/mock.ts";
 import {
@@ -10,61 +18,83 @@ import {
 } from "https://deno.land/std@0.166.0/testing/asserts.ts";
 import { _internals, buildUrl, execute } from "../src/utils.ts";
 
+configSync({ export: true });
 const BASE_URL = Deno.env.get("ENV_TYPE") === "local"
   ? "http://localhost:3000"
   : "https://serpapi.com";
 
-Deno.test("buildUrl with blank path and empty parameters", () => {
-  assertEquals(buildUrl("", {}), `${BASE_URL}?`);
+describe("buildUrl", () => {
+  let urlStub: Stub;
+
+  beforeAll(() => {
+    urlStub = stub(_internals, "getBaseUrl", () => BASE_URL);
+  });
+
+  afterAll(() => {
+    urlStub.restore();
+  });
+
+  it("with blank path and empty parameters", () => {
+    assertEquals(buildUrl("", {}), `${BASE_URL}?`);
+  });
+
+  it("with path and empty parameters", () => {
+    assertEquals(buildUrl("/", {}), `${BASE_URL}/?`);
+  });
+
+  it("with path and parameters", () => {
+    assertEquals(
+      buildUrl("/search", { q: "coffee", gl: "us" }),
+      `${BASE_URL}/search?q=coffee&gl=us`,
+    );
+  });
+
+  it("with undefined parameters", () => {
+    assertEquals(
+      buildUrl("/search", { q: "coffee", gl: undefined, hl: null }),
+      `${BASE_URL}/search?q=coffee&hl=null`,
+    );
+  });
 });
 
-Deno.test("buildUrl with path and empty parameters", () => {
-  assertEquals(buildUrl("/", {}), `${BASE_URL}/?`);
-});
-
-Deno.test("buildUrl with path and parameters", () => {
-  assertEquals(
-    buildUrl("/search", { q: "coffee", gl: "us" }),
-    `${BASE_URL}/search?q=coffee&gl=us`,
-  );
-});
-
-Deno.test("buildUrl with undefined parameters", () => {
-  assertEquals(
-    buildUrl("/search", { q: "coffee", gl: undefined, hl: null }),
-    `${BASE_URL}/search?q=coffee&hl=null`,
-  );
-});
-
-Deno.test("execute with path and parameters calls fetch with source appended", {
-  sanitizeOps: false,
-  sanitizeResources: false,
-}, async () => {
-  const fetchStub = stub(
-    _internals,
-    "fetch",
-    resolvesNext([new Response("data")]),
-  );
-  try {
-    await execute("/search", { q: "coffee", gl: "us" }, 4000);
-  } finally {
-    fetchStub.restore();
-  }
-
-  assertSpyCalls(fetchStub, 1);
-  const url = fetchStub.calls[0].args[0] as string;
-  // e.g. deno@1.28.2
-  assertMatch(
-    url,
-    /source=(nodejs|deno)%40\d+\.\d+\.\d+$/,
-  );
-});
-
-Deno.test("execute with short timeout", {
+describe("execute", {
   sanitizeOps: false,
   sanitizeResources: false,
 }, () => {
-  assertRejects(async () =>
-    await execute("/search", { q: "coffee", gl: "us" }, 1)
-  );
+  let urlStub: Stub;
+
+  beforeAll(() => {
+    urlStub = stub(_internals, "getBaseUrl", () => BASE_URL);
+  });
+
+  afterAll(() => {
+    urlStub.restore();
+  });
+
+  it("with path and parameters calls fetch with source appended", async () => {
+    const fetchStub = stub(
+      _internals,
+      "fetch",
+      resolvesNext([new Response("data")]),
+    );
+    try {
+      await execute("/search", { q: "coffee", gl: "us" }, 4000);
+    } finally {
+      fetchStub.restore();
+    }
+
+    assertSpyCalls(fetchStub, 1);
+    const url = fetchStub.calls[0].args[0] as string;
+    // e.g. deno@1.28.2
+    assertMatch(
+      url,
+      /source=(nodejs|deno)%40\d+\.\d+\.\d+$/,
+    );
+  });
+
+  it("with short timeout", () => {
+    assertRejects(async () =>
+      await execute("/search", { q: "coffee", gl: "us" }, 1)
+    );
+  });
 });
