fba0250f91ca437126ab4af3573f336055aabdf3
Sebastian Quek <sebq13@gmail.com>
2023-01-03 16:46:23 +0800

    Add descriptions for base parameters

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,42 @@
 export type BaseParameters = {
+  /**
+   * Parameter defines the device to use to get the results. It can be set to
+   * `desktop` (default) to use a regular browser, `tablet` to use a tablet browser
+   * (currently using iPads), or `mobile` to use a mobile browser (currently
+   * using iPhones).
+   */
   device?: "desktop" | "tablet" | "mobile";
+
+  /**
+   * Parameter will force SerpApi to fetch the Google results even if a cached
+   * version is already present. A cache is served only if the query and all
+   * parameters are exactly the same. Cache expires after 1h. Cached searches
+   * are free, and are not counted towards your searches per month. It can be set
+   * to `false` (default) to allow results from the cache, or `true` to disallow
+   * results from the cache. `no_cache` and `async` parameters should not be used together.
+   */
   no_cache?: boolean;
+
+  /**
+   * Parameter defines the way you want to submit your search to SerpApi. It can
+   * be set to `false` (default) to open an HTTP connection and keep it open until
+   * you got your search results, or `true` to just submit your search to SerpApi
+   * and retrieve them later. In this case, you'll need to use our
+   * [Searches Archive API](https://serpapi.com/search-archive-api) to retrieve
+   * your results. `async` and `no_cache` parameters should not be used together.
+   * `async` should not be used on accounts with
+   * [Ludicrous Speed](https://serpapi.com/plan) enabled.
+   */
   async?: boolean;
+
+  /**
+   * Parameter defines the SerpApi private key to use.
+   */
   api_key?: string | null;
+
+  /**
+   * Specify the client-side timeout of the request. In milliseconds.
+   */
   timeout?: number;
 };
 export type BaseResponse<P = Record<string | number | symbol, never>> = {
