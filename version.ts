f6459f22afab6a1afdd8f365759da55eedb0909b
Sebastian Quek <sebq13@gmail.com>
2023-01-05 13:53:44 +0800

    Add `version.ts` as the source of truth for module version

---
+++ version.ts
@@ -0,0 +1,7 @@
+/**
+ * Specifies the version of the module.
+ * Follows [semantic versioning](https://semver.org/).
+ *
+ * Changing this value creates a new release.
+ */
+export const version = "0.0.10";
