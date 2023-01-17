5edf1e8dbf0fcd2458838d67f70eb4894aedf1ad
Sebastian Quek <sebq13@gmail.com>
2023-01-17 17:00:25 +0800

    [Examples] Add basic Deno TS example

---
+++ examples/deno/basic_ts/example.ts
@@ -0,0 +1,20 @@
+import { loadSync } from "https://deno.land/std@0.173.0/dotenv/mod.ts";
+import { config, getJson, GoogleParameters } from "../../../mod.ts";
+
+const { API_KEY: apiKey } = loadSync();
+const params = {
+  q: "Coffee",
+  api_key: apiKey,
+} satisfies GoogleParameters;
+
+// Show result as JSON (async/await)
+const response1 = await getJson("google", params);
+console.log(response1["organic_results"]);
+
+// Show result as JSON (callback)
+getJson("google", params, (json) => console.log(json["organic_results"]));
+
+// Use global config
+config.api_key = apiKey;
+const response2 = await getJson("google", { q: "Coffee" });
+console.log(response2["organic_results"]);
