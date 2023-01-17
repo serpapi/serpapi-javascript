a42f336ced8a784d95b3f90918416b1233cf0686
Sebastian Quek <sebq13@gmail.com>
2023-01-17 17:08:37 +0800

    [Examples] Add basic JS CommonJS Node example

---
+++ examples/node/basic_js_commonjs/example.js
@@ -0,0 +1,26 @@
+const Dotenv = require("dotenv");
+const { config, getJson } = require("serpapi");
+
+Dotenv.config();
+const apiKey = process.env.API_KEY;
+
+const run = async () => {
+  const params = {
+    q: "Coffee",
+    api_key: apiKey,
+  };
+
+  // Show result as JSON (async/await)
+  const response1 = await getJson("google", params);
+  console.log(response1["organic_results"]);
+
+  // Show result as JSON (callback)
+  getJson("google", params, (json) => console.log(json["organic_results"]));
+
+  // Use global config
+  config.api_key = apiKey;
+  const response2 = await getJson("google", { q: "Coffee" });
+  console.log(response2["organic_results"]);
+};
+
+run();
