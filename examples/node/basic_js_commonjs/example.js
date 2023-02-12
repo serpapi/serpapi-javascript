d6eb40800170c3b4da47f96fe110a0af7d2ec4f0
Sebastian Quek <sebq13@gmail.com>
2023-02-12 20:44:33 +0800

    Update examples

--- examples/node/basic_js_commonjs/example.js
+++ examples/node/basic_js_commonjs/example.js
@@ -14,13 +14,17 @@ const run = async () => {
   const response1 = await getJson("google", params);
   console.log(response1["organic_results"]);
 
+  // Show result as JSON (async/await with search params as first argument)
+  const response2 = await getJson({ engine: "google", ...params });
+  console.log(response2["organic_results"]);
+
   // Show result as JSON (callback)
   getJson("google", params, (json) => console.log(json["organic_results"]));
 
   // Use global config
   config.api_key = apiKey;
-  const response2 = await getJson("google", { q: "Coffee" });
-  console.log(response2["organic_results"]);
+  const response3 = await getJson("google", { q: "Coffee" });
+  console.log(response3["organic_results"]);
 };
 
 run();
--- examples/node/basic_ts_esm/example.ts
+++ examples/node/basic_ts_esm/example.ts
@@ -13,10 +13,14 @@ const params = {
 const response1 = await getJson("google", params);
 console.log(response1["organic_results"]);
 
+// Show result as JSON (async/await with search params as first argument)
+const response2 = await getJson({ engine: "google", ...params });
+console.log(response2["organic_results"]);
+
 // Show result as JSON (callback)
 getJson("google", params, (json) => console.log(json["organic_results"]));
 
 // Use global config
 config.api_key = apiKey;
-const response2 = await getJson("google", { q: "Coffee" });
-console.log(response2["organic_results"]);
+const response3 = await getJson("google", { q: "Coffee" });
+console.log(response3["organic_results"]);
